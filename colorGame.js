448c8464261431ba64ce02b0bf7134f3baa5287f
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-07-20 15:35:39 -0700

    Wrote out coding steps in plain, commented out english

---
+++ colorGame.js
@@ -0,0 +1,14 @@
+/* Steps:
+ * Initial Setup
+ * 	1. Generate a random rgb color code on page load
+ *  2. Assign color code to HTML <div> square
+ *  3. Generate and assign color codes for n squares (n = 3, initially)
+ *  4. Select one color from generated colors and print rgb code in HTML header
+ * Game Play
+ *  5. When user clicks square, check if square color equals printed color
+ *  6. If no, prompt user to guess again. Hide square.
+ *  7. If yes, log/alert win message and end game.
+ * Game Controls
+ *	8. Add "new game" button. Regenerate colors when button is clicked. Change text content when game ends.
+ * 	9. Add "easy"/"hard" button options. Buttons set number of squares, n (3 or 6).
+ */
