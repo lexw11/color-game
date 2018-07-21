bb052d1bad77103249e57e1a74d1a26d67f21297
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-07-21 10:33:55 -0700

    Factored code into smaller functions

--- colorGame.js
+++ colorGame.js
@@ -20,99 +20,68 @@
 
 // Declare and initialize variables
 var numSquares = 3, // Default is "easy" mode
-	random,
-	rgb = [0, 0, 0], // 3 item array of numbers, initially filled with 0s 
-	rgbStr,
-	colorPrompt,
-	correctIndex,
 	correctColor;
 
-// Get HTML objects
-var squares = document.getElementsByClassName( "square" ),
-	message = document.getElementById( "message" ),
-	playAgain = document.getElementById( "play-again" ),
-	easyButton = document.getElementById( "easy" ),
-	hardButton = document.getElementById( "hard" );
+// Query HTML objects
+var squares 		= document.getElementsByClassName( "square" ),
+	colorPrompt 	= document.getElementById( "color-prompt" ),
+	message 		= document.getElementById( "message" ),
+	restartButton 	= document.getElementById( "restart" ),
+	easyButton 		= document.getElementById( "easy" ),
+	hardButton 		= document.getElementById( "hard" );
 
+// Start new game
 newGame();
 
-
-// Event Listeners
+// ========================
+// Add Event Listeners
+// ========================
 // Loop through squares
 for ( var i = 0; i < squares.length; i++ ) {
-	// Add click event listener for each square
+	// Test if square is correct color when clicked
 	squares[i].addEventListener( "click", function() {
 		testSquare( this );
 	});
 }
 
 // Restart game when button is clicked
-playAgain.addEventListener( "click", function() {
+restartButton.addEventListener( "click", function() {
 	newGame();
 })
 
 // Select difficulty mode
 easyButton.addEventListener( "click", function() {
 	numSquares = 3;
+	// Restart game
 	newGame();
 });
 
 hardButton.addEventListener( "click", function() {
 	numSquares = 6;
+	// Restart game
 	newGame();
 });
 
-
+// ========================
 // Functions
+// ========================
 function newGame() {
-	var colorsArr = [];
-	// Loop through squares
-	for( var i = 0; i < numSquares; i++ ) {
-		// Loop through r-g-b numbers
-		for( var j = 0; j < rgb.length; j++ ) {
+	// Show squares and get array of colors
+	var colors = colorSquares( squares, numSquares );
 
-			// Get random number between 0 and 255
-			random = Math.floor( Math.random() * 256 );
-			// Assign number to current rgb array index
-			rgb[j] = random;
-		}
-		// Create rgb string for new rgb array
-		rgbStr = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
-		// Add string to array
-		colorsArr.push( rgbStr );
-		// Assign square color
-		squares[i].style.backgroundColor = rgbStr;
-		// Make squares visible
-		squares[i].style.opacity = 1;
-	}
-
-	// Loop through unused squares (if in "easy" mode)
-	for( var i = numSquares; i < squares.length; i++) {
-		// Hide visible
-		squares[i].style.opacity = 0;
-	}
-
-	// Select a color from the assigned colors as the correct color
-	correctIndex = Math.floor( Math.random() * numSquares );
-	correctColor = colorsArr[ correctIndex ];
-
-	// Get color-prompt HTML object
-	colorPrompt = document.getElementById( "color-prompt" );
-
-	// Display correct color rgb code
-	colorPrompt.textContent = correctColor;
+	// Pick color prompt from colors array
+	getColorPrompt( colors, numSquares );
 
 	// Clear text from message element
 	message.textContent = "";
 
-	// Reset play again button text
-	playAgain.textContent = "New Game";
+	// Reset restart button text
+	restartButton.textContent = "New Game";
 }
 
 function testSquare( square ) {
 	// Check if square shows the rgb code color
 	if ( square.style.backgroundColor == correctColor ) {
-		
 		// All squares become the correct color
 		for ( var j = 0; j < numSquares; j++ ) {
 			squares[j].style.backgroundColor = correctColor;
@@ -122,14 +91,70 @@ function testSquare( square ) {
 		message.textContent = "You win!";
 
 		// Prompt play again
-		playAgain.textContent = "Play again?";
+		restartButton.textContent = "Play again?";
 
 	} else {
-		
 		// Square disappears
 		square.style.opacity = 0;
 
 		// Show win message
 		message.textContent = "Try again";
 	}
+}
+
+// Generates random RGB color string
+function randomRgb() {
+	var random,
+		rgb = [0, 0, 0], // 3 item array, initially filled with 0s 
+		rgbStr;
+
+	// Loop through r-g-b numbers
+	for( var j = 0; j < rgb.length; j++ ) {
+		// Get random number between 0 and 255
+		random = Math.floor( Math.random() * 256 );
+		// Assign number to current rgb array index
+		rgb[j] = random;
+	}
+
+	// Create rgb string for new rgb array
+	rgbStr = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
+	return rgbStr;
+}
+
+// Shows and colors squares for new game. Returns array of colors
+function colorSquares( squares, numSquares ) {
+	var newRgb,
+		colorsArr = [];
+
+	for( var i = 0; i < squares.length; i++) {
+		if( i < numSquares ) {
+			// Make squares visible
+			squares[i].style.opacity = 1;
+
+			// Create new rgb string
+			newRgb = randomRgb();
+			// Assign color to square
+			squares[i].style.backgroundColor = newRgb;
+
+			// Add color to array
+			colorsArr.push( newRgb );
+
+		} else {
+			// Hide visible
+			squares[i].style.opacity = 0;	
+		}
+	}
+	return colorsArr;
+}
+
+// Randomly selects and displays the color prompt from the color array
+function getColorPrompt( colorsArr, numSquares ) {
+	var correctIndex;
+	
+	// Select a color from the assigned colors as the correct color
+	correctIndex = Math.floor( Math.random() * numSquares );
+	correctColor = colorsArr[ correctIndex ];
+
+	// Display correct color rgb code
+	colorPrompt.textContent = correctColor;
 }
